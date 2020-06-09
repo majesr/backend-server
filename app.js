79ef74a7e8e9b90cc057cdaa9016d0f1c33c6eac
Manuel Sernaque <majesr1195@gmail.com>
2020-06-08 23:34:05 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 
-// Inicializar variable
+// Inicializar variables
 var app = express();
 
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 // Conexion a la base de datos
-mongoose.connection.openUri('mongodb://localhost:2717/hospitalDB', (err, res) => {
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
     if (err) throw err;
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
 });
 
-//Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente '
-    });
+/// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-});
 
 // Escuchar el express o peticiones
 app.listen(3000, () => {
