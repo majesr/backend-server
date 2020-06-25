d7e39a5bee20e31d44b96aed232a751b7c902edd
Manuel Sernaque <majesr1195@gmail.com>
2020-06-25 01:05:49 -0500

    Seccion 11 terminada - CRUD- Imagenes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 
 // Inicializar variables
@@ -18,6 +18,11 @@ app.use(bodyParser.json())
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -26,9 +31,21 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 });
 
+// Server index config
+//var serveIndex = require('serve-index');
+//app.use(express.static(__dirname + '/'))
+//app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
+
 /// Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
+
 app.use('/', appRoutes);
 
 
--- routes/usuario.js
+++ routes/usuario.js
@@ -11,8 +11,12 @@ var Usuario = require('../models/usuario');
 //Obtener todos los usuarios
 //====================================
 app.get('/', (req, res, next) => {
+    var desde = req.query.desde || 0;
+    desde = Number(desde);
 
     Usuario.find({}, 'nombre email img role')
+        .skip(desde)
+        .limit(5)
         .exec(
             (err, usuarios) => {
                 if (err) {
@@ -22,10 +26,13 @@ app.get('/', (req, res, next) => {
                         errors: err
                     });
                 }
+                Usuario.count({}, (err, conteo) => {
+                    res.status(200).json({
+                        ok: true,
+                        usuarios: usuarios,
+                        total: conteo
+                    });
 
-                res.status(200).json({
-                    ok: true,
-                    usuarios: usuarios
                 });
 
             });
